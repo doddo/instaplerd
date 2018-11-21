72b9c67903744ca3d13abe1b2a9964033ef22a27
Petter hassberg <dr.doddo@gmail.com>
2018-11-21 23:31:11 +0100

    Tune some Filters

--- lib/InstaPlerd/Filters/Artistic.pm
+++ lib/InstaPlerd/Filters/Artistic.pm
@@ -19,7 +19,6 @@ sub _apply {
 
     $source_image->Border(geometry => '14x14', color => 'white');
     $source_image->Border(geometry => '1x1', color => 'gray');
-    $source_image->Set(type => 'grayscale');
 
 }
 
--- lib/InstaPlerd/Filters/Batman.pm
+++ lib/InstaPlerd/Filters/Batman.pm
@@ -15,7 +15,7 @@ sub _apply{
     my $source_image = shift;
 
     $source_image->Modulate(brightness => 110, saturation => 60, hue => 110);
-    $source_image->Gamma(0.6);
+    $source_image->Gamma(0.8);
     $source_image->Colorize(fill => '#222b6d', blend=> '20' );
 
     $source_image->Contrast(sharpen=>'True');
