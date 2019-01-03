ac30d7b0312fd011e8513a16d58c68966237b922
Petter hassberg <dr.doddo@gmail.com>
2019-01-04 00:25:29 +0100

    Make Batman filter work on IM6

--- lib/InstaPlerd/Filters/Batman.pm
+++ lib/InstaPlerd/Filters/Batman.pm
@@ -10,14 +10,26 @@ extends "InstaPlerd::Filter";
 
 sub _apply {
     my $self = shift;
+
     my $source_image = shift;
 
-    $source_image->Modulate(brightness => 110, saturation => 60, hue => 110);
+    $source_image->Modulate(brightness => 110, saturation => 50, hue => 110);
     $source_image->Gamma(0.8);
-    $source_image->Colorize(fill => '#222b6d', blend => '20');
 
-    $source_image->Contrast(sharpen => 'True');
-    $source_image->Contrast(sharpen => 'True');
+    my $gray = $source_image->Clone();
+    $gray->Colorspace(colorspace => 'Gray');
+
+    my $blue = $gray->Clone();
+
+    $blue->Colorize(fill => '#222b6d', blend => "100%");
+
+    $blue->Composite(compose => 'Blend', image => $gray);
+
+    $source_image->Set("compose:args", "40");
+    $source_image->Composite(compose => 'Blend', image => $blue, blend => "40");
+
+    $source_image->Contrast(sharpen => 'true');
+    $source_image->Contrast(sharpen => 'true');
 
     $source_image->Shave(geometry => '20x20');
     $source_image->Border(geometry => '20x20', color => 'black');
