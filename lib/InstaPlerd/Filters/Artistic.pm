9556806bbb225d0d684b5c860fe4f05c933aedcb
Petter hassberg <dr.doddo@gmail.com>
2019-05-01 00:27:40 +0200

    Make borders thicker

--- lib/InstaPlerd/Filters/Artistic.pm
+++ lib/InstaPlerd/Filters/Artistic.pm
@@ -15,9 +15,9 @@ sub _apply {
 
     $source_image->Modulate(brightness => 110, saturation => 110, hue => 110);
     $source_image->ContrastStretch('5%');
-    $source_image->Shave(geometry => '15x15');
+    $source_image->Shave(geometry => '30x30');
 
-    $source_image->Border(geometry => '14x14', color => 'white');
+    $source_image->Border(geometry => '29x29', color => 'white');
     $source_image->Border(geometry => '1x1', color => 'gray');
 
     return $source_image;
